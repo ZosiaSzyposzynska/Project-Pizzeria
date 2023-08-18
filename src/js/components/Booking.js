e2130c60481a0978d88ba51d1b658f781d854613
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-08-18 10:26:14 +0200

    Add changes Booking js

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,32 +1,42 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
-class Booking{
-    constructor(element){
+
+class Booking {
+    constructor(element) {
+        
         const thisBooking = this;
-        thisBooking.render(element);
-        thisBooking.initWidgets;
 
+    
+        thisBooking.render(element);
+        thisBooking.initWidgets(); 
+
+        console.log('Booking:', thisBooking);
     }
 
-    render(element){
+    render(element) {
         const thisBooking = this;
-        /* generate HTML */
         const generatedHTML = templates.bookingWidget();
-        /* empty object */
         thisBooking.dom = {};
-        /* add wrapper to thisBooking object */
         thisBooking.dom.wrapper = element;
         thisBooking.dom.wrapper.innerHTML = generatedHTML;
-        /* select peopleAmount */
         thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
-        /* select hoursAmount */
         thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+        thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
     }
 
+    initWidgets() {
+        const thisBooking = this;
 
-
+        thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
+        thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+        thisBooking.datePickerWidget = new DatePicker(thisBooking.dom.datePicker); 
+        thisBooking.hourPickerWidget = new HourPicker(thisBooking.dom.hourPicker); 
+        
+    }
 }
-
 
 export default Booking;
